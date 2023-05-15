0977fa6d15622b5cc361d5640a6b0b4345bb7a37
noveshgoyal <goyalnovesh@gmail.com>
2023-05-16 00:27:59 +0530

    style- footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <>
       <Routes >
         <Route path="/" element={<Layout />} />
       </Routes>
+      <Footer/>
+      
+      </>
   );
 }
 
